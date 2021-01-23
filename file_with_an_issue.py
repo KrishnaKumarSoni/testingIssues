210547ff44bd4504537b701a2ba348524340364d
KrishnaKumarSoni <39898943+KrishnaKumarSoni@users.noreply.github.com>
2021-01-23 13:24:31 +0530

    Update file_with_an_issue.py

--- file_with_an_issue.py
+++ file_with_an_issue.py
@@ -7,6 +7,5 @@
 #this is not an issue. All good.
 #this is not an issue. All good.
 #this is not an issue. All good.
-#this is an issue.
 #this is not an issue. All good.
 #this is not an issue. All good.
